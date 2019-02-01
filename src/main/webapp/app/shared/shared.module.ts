5a0f5020a93514640f2264a7a7c66bd5a6b43817
spires <spires@wemanity.com>
2019-02-01 23:32:01 +0100

    Adding material angular support !

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 
 import { NgbDateMomentAdapter } from './util/datepicker-adapter';
 import { MyAppSharedLibsModule, MyAppSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
+import { MaterialAngularModule } from 'app/material-angular.module';
 
 @NgModule({
-    imports: [MyAppSharedLibsModule, MyAppSharedCommonModule],
+    imports: [MyAppSharedLibsModule, MyAppSharedCommonModule, MaterialAngularModule],
     declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective],
     providers: [{ provide: NgbDateAdapter, useClass: NgbDateMomentAdapter }],
     entryComponents: [JhiLoginModalComponent],
-    exports: [MyAppSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective],
+    exports: [MyAppSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, MaterialAngularModule],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MyAppSharedModule {
